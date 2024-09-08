ba1c2b78c52e04a77e83b67c7b966985626e6947
SquaringHard <umr091105@gmail.com>
2024-09-08 19:58:39 +0700

    add compile guard

--- getExpectedLength.cpp
+++ getExpectedLength.cpp
@@ -1,59 +1,65 @@
-// #include "ft.h"
-// #include <utility>      // pair
-// #include <string>
-// #include <fstream>      // ifstream, ofstream
-// #include <cmath>        // INFINITY
-// #include <filesystem>
-// #include <iostream>     // cin, cout
-// #include <sstream>      // istringstream
-// #include <iterator>     // istream_iterator
-// #include <iomanip>      // fixed, setprecision
-// #include <limits>       // numeric_limits
+// only compile this when results in expected folder are incorrect or missing and you need to re-run getExpectedLength.exe
 
-// int main() {
-//     cout << "Enter the filenames (in folder \"input\"), separated by spaces, or press Enter to run all files: ";
-//     string filenames;
-//     getline(cin, filenames);
+// #define I_AM_READY_TO_COMPILE_THIS_FILE
 
-//     istringstream iss(filenames);
-//     vector<string> filenamesVec(istream_iterator<string>{iss}, istream_iterator<string>{});
-//     if (filenamesVec.empty()) for (const auto &entry : filesystem::directory_iterator("input")) filenamesVec.push_back(entry.path().filename().string());
+#ifdef I_AM_READY_TO_COMPILE_THIS_FILE
+#include "ft.h"
+#include <utility>      // pair
+#include <string>
+#include <fstream>      // ifstream, ofstream
+#include <cmath>        // INFINITY
+#include <filesystem>
+#include <iostream>     // cin, cout
+#include <sstream>      // istringstream
+#include <iterator>     // istream_iterator
+#include <iomanip>      // fixed, setprecision
+#include <limits>       // numeric_limits
 
-//     const size_t startIndex = 0;
-//     for (const string& filename : filenamesVec) {
-//         const string realFilename = string(filename) + "_s=" + to_string(startIndex) + ".txt"; 
-//         cout << "\nMaking " << realFilename << " ...";
+int main() {
+    cout << "Enter the filenames (in folder \"input\"), separated by spaces, or press Enter to run all files: ";
+    string filenames;
+    getline(cin, filenames);
 
-//         ifstream input(string("input/") + filename);
-//         size_t v, f, e;
-//         input >> v >> f >> e;
+    istringstream iss(filenames);
+    vector<string> filenamesVec(istream_iterator<string>{iss}, istream_iterator<string>{});
+    if (filenamesVec.empty()) for (const auto &entry : filesystem::directory_iterator("input")) filenamesVec.push_back(entry.path().filename().string());
 
-//         vector<Point> points;
-//         for (size_t i = 0; i < v; i++) {
-//             double x, y, z;
-//             input >> x >> y >> z;
-//             points.emplace_back(x, y, z, i);
-//         }
+    const size_t startIndex = 0;
+    for (const string& filename : filenamesVec) {
+        const string realFilename = string(filename) + "_s=" + to_string(startIndex) + ".txt"; 
+        cout << "\nMaking " << realFilename << " ...";
 
-//         vector<array<indexType, 3>> trianglesPointsIndexes;
-//         for (size_t i = 0; i < f; i++) {
-//             indexType a, b, c;
-//             short three;
-//             input >> three >> a >> b >> c;
-//             trianglesPointsIndexes.push_back({a, b, c});
-//         }
+        ifstream input(string("input/") + filename);
+        size_t v, f, e;
+        input >> v >> f >> e;
 
-//         const vector<Funnel*> list = FunnelTree(TriangleMesh(points, trianglesPointsIndexes), points.at(startIndex));
+        vector<Point> points;
+        for (size_t i = 0; i < v; i++) {
+            double x, y, z;
+            input >> x >> y >> z;
+            points.emplace_back(x, y, z, i);
+        }
 
-//         vector<double> shortestLength(v, INFINITY);
-//         shortestLength[startIndex] = 0;
-//         for (const Funnel *const f : list) if (shortestLength[f->p->index] > f->sp) shortestLength[f->p->index] = f->sp;
+        vector<array<indexType, 3>> trianglesPointsIndexes;
+        for (size_t i = 0; i < f; i++) {
+            indexType a, b, c;
+            short three;
+            input >> three >> a >> b >> c;
+            trianglesPointsIndexes.push_back({a, b, c});
+        }
 
-//         filesystem::create_directory("expected");
-//         ofstream output(string("expected/") + realFilename);
-//         output << fixed << setprecision(numeric_limits<double>::max_digits10);
-//         for (const double i : shortestLength) output << i << '\n';
+        const vector<Funnel*> list = FunnelTree(TriangleMesh(points, trianglesPointsIndexes), points.at(startIndex));
 
-//         deleteFunnelTree(list);
-//     }
-// }
+        vector<double> shortestLength(v, INFINITY);
+        shortestLength[startIndex] = 0;
+        for (const Funnel *const f : list) if (shortestLength[f->p->index] > f->sp) shortestLength[f->p->index] = f->sp;
+
+        filesystem::create_directory("expected");
+        ofstream output(string("expected/") + realFilename);
+        output << fixed << setprecision(numeric_limits<double>::max_digits10);
+        for (const double i : shortestLength) output << i << '\n';
+
+        deleteFunnelTree(list);
+    }
+}
+#endif
